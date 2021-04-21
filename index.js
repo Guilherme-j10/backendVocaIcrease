0f3bb08d2f1f6b220b9660c168aae75aa70578fb
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2021-04-21 16:28:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/translate', async (req, res) => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://translate.google.com');
+  
+  await page.type('textarea[jsname=BJE2fc]', req.body.word, { delay: 10 });
+  setTimeout(async () => {
+    try {
+      const StringContent = await page.$eval('span[jsname=W297wb]', el => el.textContent);
+      res.json(`${StringContent}`);
+      browser.close();
+    } catch (error) {
+      const AnotherString = await page.$eval('span[jsname=jqKxS]', el => el.textContent);
+      res.json(`${AnotherString} / feminino|masculino`);
+      browser.close();
+    }
+  }, 2000);
+})
+
+app.listen(3232, () => {
+  console.log('Acesse em http://localhost:3232');
+})
